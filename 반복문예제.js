16a778922200e75a49b8cad06bbfff0e9d405c6c
conqueror-G <gitconquerorg@gmail.com>
2021-11-13 16:27:23 +0900

    반복문 1번 예제 업로드

---
+++ 반복문예제.js
@@ -0,0 +1,14 @@
+// 1번
+
+function calculate(number) {
+  const numData = [];
+  for (let i = 0; i <= bigData.length; i++) {
+    if ( i > 3 ) {
+      numData.push(i);
+    }
+  }
+  return numData;
+};
+
+const bigData = [1,2,3,4,5,6,7];
+console.log(calculate(number));
